516e172a04d648f95bc089fd5ea6b5a65da64a5c
RichardRanft <vegassparkyrich@gmail.com>
2014-12-03 11:00:01 -0800

    Update to project layout

---
+++ SimpleHttpServer/HttpServer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Bend.Util
+{
+    public abstract class HttpServer
+    {
+        protected int m_port;
+        protected IPAddress m_address;
+        TcpListener listener;
+        bool is_active = true;
+        protected String m_httpRoot;
+
+        public String HTTPRoot
+        {
+            get
+            {
+                return m_httpRoot;
+            }
+            set
+            {
+                m_httpRoot = value;
+            }
+        }
+
+        public HttpServer(int port)
+        {
+            this.m_port = port;
+        }
+
+        public HttpServer(byte[] address, int port)
+        {
+            this.m_address = new IPAddress(address);
+            this.m_port = port;
+        }
+
+        public void listen()
+        {
+            if (m_address == null)
+            {
+                byte[] ipParts = { 127, 0, 0, 1 };
+                m_address = new IPAddress(ipParts);
+            }
+            listener = new TcpListener(m_address, m_port);
+            listener.Start();
+            while (is_active)
+            {
+                TcpClient s = listener.AcceptTcpClient();
+                HttpProcessor processor = new HttpProcessor(s, this);
+                Thread thread = new Thread(new ThreadStart(processor.process));
+                thread.Start();
+                Thread.Sleep(1);
+            }
+        }
+
+        public abstract void handleGETRequest(HttpProcessor p);
+        public abstract void handlePOSTRequest(HttpProcessor p, StreamReader inputData);
+    }
+}
---
+++ SimpleHttpServer/MyHttpServer.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Bend.Util
+{
+    public class MyHttpServer : HttpServer
+    {
+        public MyHttpServer(int port)
+            : base(port)
+        {
+        }
+
+        public MyHttpServer(byte[] address, int port)
+            : base(address, port)
+        {
+        }
+
+        public override void handleGETRequest(HttpProcessor p)
+        {
+            String url = p.http_url.ToLower();
+            if (url.Contains(".png"))
+            {
+                Stream fs = File.Open(HTTPRoot + url, FileMode.Open);
+
+                p.writeSuccess("image/png");
+                fs.CopyTo(p.outputStream.BaseStream);
+                p.outputStream.BaseStream.Flush();
+                fs.Close();
+            }
+
+            if (url.Contains(".jpg"))
+            {
+                Stream fs = File.Open(HTTPRoot + url, FileMode.Open);
+
+                p.writeSuccess("image/jpg");
+                fs.CopyTo(p.outputStream.BaseStream);
+                p.outputStream.BaseStream.Flush();
+                fs.Close();
+            }
+
+            if (url.Contains(".pdb") || url.Contains(".exe") || url.Contains(".dll"))
+            {
+                Stream fs = File.Open(HTTPRoot + url, FileMode.Open);
+
+                p.writeSuccess("application/octet-stream");
+                fs.CopyTo(p.outputStream.BaseStream);
+                p.outputStream.BaseStream.Flush();
+                fs.Close();
+            }
+
+            Console.WriteLine("request: {0}", url);
+
+            String page = getPage(url);
+            if (page != "")
+            {
+                p.writeSuccess();
+                p.outputStream.Write(page);
+            }
+            else
+            {
+                // send test page
+                p.writeFailure();
+            }
+        }
+
+        public override void handlePOSTRequest(HttpProcessor p, StreamReader inputData)
+        {
+            Console.WriteLine("POST request: {0}", p.http_url);
+            string data = inputData.ReadToEnd();
+
+            p.writeSuccess();
+            p.outputStream.WriteLine("<html><body><h1>test server</h1>");
+            p.outputStream.WriteLine("<a href=/test>return</a><p>");
+            p.outputStream.WriteLine("postbody: <pre>{0}</pre>", data);
+        }
+
+        private String getPage(String pageName)
+        {
+            pageName = pageName.Replace("/", "");
+            if (pageName == "")
+                return "";
+            String[] files = Directory.GetFiles(m_httpRoot);
+            String page = "";
+            foreach (String file in files)
+            {
+                if (file.Contains(pageName))
+                {
+                    StreamReader reader = new StreamReader(file);
+                    page = reader.ReadToEnd();
+                }
+            }
+            return page;
+        }
+    }
+}
